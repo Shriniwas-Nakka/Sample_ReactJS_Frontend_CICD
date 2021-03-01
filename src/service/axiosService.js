18cd94a6f2ff9435984d56ba2f53b594a8fee7a5
Shriniwas-Nakka <nakkashriniwas1@gmail.com>
2021-03-01 23:13:40 +0530

    ADDED ip

--- src/service/axiosService.js
+++ src/service/axiosService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default class AxiosService {
 
-    baseUrl = "http://localhost:5000/"
+    baseUrl = "http://3.138.244.133:5000/"
 
     post = (url, data, header = false) => {
         return axios.post(`${this.baseUrl}${url}`, data, header)
